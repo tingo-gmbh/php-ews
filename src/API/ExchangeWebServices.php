850831edae070a108c115e133f856d7f1c49e9ed
Garethp <blackfireweb@gmail.com>
2016-05-09 15:48:00 +0200

    Removing unused properties and improving version decetion

--- src/API/ExchangeWebServices.php
+++ src/API/ExchangeWebServices.php
@@ -28,54 +28,18 @@ use jamesiarmes\PEWS\API\Type\EmailAddressType;
  */
 class ExchangeWebServices
 {
-    /**
-     * Microsoft Exchange 2007
-     *
-     * @var string
-     */
     const VERSION_2007 = 'Exchange2007';
-
-    /**
-     * Microsoft Exchange 2007 SP1
-     *
-     * @var string
-     */
     const VERSION_2007_SP1 = 'Exchange2007_SP1';
-
-    /**
-     * Microsoft Exchange 2007 SP2
-     *
-     * @var string
-     */
     const VERSION_2007_SP2 = 'Exchange2007_SP2';
-
-    /**
-     * Microsoft Exchange 2007 SP3
-     *
-     * @var string
-     */
     const VERSION_2007_SP3 = 'Exchange2007_SP3';
 
-    /**
-     * Microsoft Exchange 2010
-     *
-     * @var string
-     */
     const VERSION_2010 = 'Exchange2010';
-
-    /**
-     * Microsoft Exchange 2010 SP1
-     *
-     * @var string
-     */
     const VERSION_2010_SP1 = 'Exchange2010_SP1';
-
-    /**
-     * Microsoft Exchange 2010 SP2
-     *
-     * @var string
-     */
     const VERSION_2010_SP2 = 'Exchange2010_SP2';
+    const VERSION_2010_SP3 = 'Exchange2010_SP3';
+
+    const VERSION_2013 = 'Exchange2013';
+    const VERSION_2013_SP1 = 'Exchange2013_SP1';
 
     /**
      * Password to use when connecting to the Exchange server.
