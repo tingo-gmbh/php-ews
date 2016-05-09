300f09752d5d0c98b88ce9b303e21b6c848a1bec
Jannik <j.waschkau@boewa.de>
2016-05-09 15:24:48 +0200

    added doc for creating addresses

--- examples/contacts/createContacts.php
+++ examples/contacts/createContacts.php
@@ -1,6 +1,7 @@
 <?php
 
-use jamesiarmes\PEWS\Contacts\ContactsAPI as API;
+use garethp\ews\Contacts\ContactsAPI as API;
+use \garethp\ews\API\Enumeration;
 
 $api = API::withUsernameAndPassword('server', 'username', 'password');
 
@@ -8,9 +9,23 @@ $api->createContacts(array (
     'GivenName' => 'John',
     'Surname' => 'Smith',
     'EmailAddresses' => array(
-        'Entry' => array('Key' => 'EmailAddress1', '_value' => 'john.smith@gmail.com')
+        'Entry' => array('Key' => Enumeration\EmailAddressKeyType::EMAIL_ADDRESS_1, '_value' => 'john.smith@gmail.com')
     ),
+    //Creating multiple entries
     'PhoneNumbers' => array(
-        'Entry' => array('Key' => 'HomePhone', '_value' => '000')
-    )
+        'Entry' => array(
+            array('Key' => Enumeration\PhoneNumberKeyType::HOME_PHONE, '_value' => '000'),
+            array('Key' => Enumeration\PhoneNumberKeyType::BUSINESS_PHONE, '_value' => '111'),
+        )
+    ),
+    'PhysicalAddresses' => array(
+        'Entry' => array(
+                 'Key' => Enumeration\PhysicalAddressKeyType::HOME,
+                 'street' => '123 Street',
+                 'city' => '123 City',
+                 'state' => '123 State',
+                 'countryOrRegion' => '123 Country',
+                 'postalCode' => '12345',
+             )
+    ),
 ));
