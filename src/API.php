7c707d6a3ea408acda57b7eee12c0c61301525c1
Garethp <blackfireweb@gmail.com>
2016-05-10 23:28:36 +0200

    Moved FieldURI logic in to it's own class, and deprecated certain functions

--- src/API.php
+++ src/API.php
@@ -12,6 +12,7 @@ use garethp\ews\API\Message\UpdateItemResponseMessageType;
 use garethp\ews\API\Type;
 use garethp\ews\Calendar\CalendarAPI;
 use garethp\ews\Mail\MailAPI;
+use garethp\ews\API\FieldURIManager;
 
 /**
  * A base class for APIs
@@ -40,9 +41,6 @@ class API
         return $this->getClient()->getPrimarySmtpMailbox();
     }
 
-    private $unIndexedFieldUris = array();
-    private $dictionaryFieldUris = array();
-
     /**
      * Storing the API client
      * @var ExchangeWebServices
@@ -50,113 +48,46 @@ class API
     private $client;
 
     /**
+     * @deprecated This will be removed in 0.9
+     *
      * @param $className
      * @return array
      */
     public function getFieldUrisFromClass($className)
     {
-        //So, since we have to pass in URI's of everything we update, we need to fetch them
-        $reflection = new \ReflectionClass($className);
-        $constants = $reflection->getConstants();
-        $constantsFound = array();
-
-        //Loop through all URI's to list them in an array
-        foreach ($constants as $constant) {
-            $exploded = explode(":", $constant);
-            if (count($exploded) == 1) {
-                $exploded = ['item', $exploded[0]];
-            }
-
-            $name = strtolower($exploded[1]);
-            $category = strtolower($exploded[0]);
-
-            if (!isset($constantsFound[$name])) {
-                $constantsFound[$name] = array();
-            }
-            if (count($exploded) == 3) {
-                $entry = strtolower($exploded[2]);
-                if (!isset($constantsFound[$name])) {
-                    $constantsFound[$name][$category] = array();
-                }
-
-                $constantsFound[$name][$category][$entry] = $constant;
-            } else {
-                $constantsFound[$name][$category] = $constant;
-            }
-        }
-
-        return $constantsFound;
+        return FieldURIManager::getFieldUrisFromClass($className);
     }
 
     public function setupFieldUris()
     {
-        $this->unIndexedFieldUris = $this
-            ->getFieldUrisFromClass(UnindexedFieldURIType::class);
-
-        $this->dictionaryFieldUris = $this
-            ->getFieldUrisFromClass(DictionaryURIType::class);
+        FieldURIManager::setupFieldUris();
     }
 
+    /**
+     * @deprecated This will be removed in 0.9. See FieldURIManager
+     *
+     * @param $fieldName
+     * @param string $preference
+     * @throws ExchangeException
+     * @return string
+     */
     public function getFieldUriByName($fieldName, $preference = 'item')
     {
-        $fieldName = strtolower($fieldName);
-        $preference = strtolower($preference);
-
-        if (empty($this->unIndexedFieldUris)) {
-            $this->setupFieldUris();
-        }
-
-        if (!isset($this->unIndexedFieldUris[$fieldName])) {
-            return false;
-        }
-
-        if (!isset($this->unIndexedFieldUris[$fieldName][$preference])) {
-            $preference = 'item';
-        }
-
-        if (!isset($this->unIndexedFieldUris[$fieldName][$preference])) {
-            throw new ExchangeException("Could not find uri $preference:$fieldName");
-        }
-
-        return $this->unIndexedFieldUris[$fieldName][$preference];
+        return FieldURIManager::getFieldUriByName($fieldName, $preference);
     }
 
+    /**
+     * @deprecated This will be removed in 0.9. See FieldURIManager
+     *
+     * @param $fieldName
+     * @param string $preference
+     * @param bool $entryKey
+     * @throws ExchangeException
+     * @return string
+     */
     public function getIndexedFieldUriByName($fieldName, $preference = 'item', $entryKey = false)
     {
-        $fieldName = strtolower($fieldName);
-        $preference = strtolower($preference);
-
-        if (empty($this->dictionaryFieldUris)) {
-            $this->setupFieldUris();
-        }
-
-        if (!isset($this->dictionaryFieldUris[$fieldName])) {
-            return false;
-        }
-
-        if (!isset($this->dictionaryFieldUris[$fieldName][$preference])) {
-            $preference = 'item';
-        }
-
-        if (!isset($this->dictionaryFieldUris[$fieldName][$preference])) {
-            throw new ExchangeException("Could not find uri $preference:$fieldName");
-        }
-
-        $fieldUri = $this->dictionaryFieldUris[$fieldName][$preference];
-        if (is_array($fieldUri)) {
-            if (!$entryKey) {
-                throw new ExchangeException("Please enter a specific entry key for this fieldURI");
-            }
-
-            $entryKey = strtolower($entryKey);
-            if (!isset($fieldUri[$entryKey])) {
-                throw new ExchangeException("Could not find uri $preference:$fieldName:$entryKey");
-            }
-
-            $fieldUri = $fieldUri[$entryKey];
-        }
-
-        return $fieldUri;
+        return FieldURIManager::getIndexedFieldUriByName($fieldName, $preference, $entryKey);
     }
 
     /**
