7678bb5f3a761107e1a020b69221670a83ea4730
Paulo Pinto <paulo@teamleader.eu>
2017-03-20 16:18:13 +0000

    Attach response to ExchangeException when getting items

--- src/API/Exception/ExchangeException.php
+++ src/API/Exception/ExchangeException.php
@@ -3,8 +3,28 @@
 namespace garethp\ews\API\Exception;
 
 use garethp\ews\API;
+use garethp\ews\API\Message\ResponseMessageType;
 
 class ExchangeException extends API\Exception
 {
+    /**
+     * @var ResponseMessageType
+     */
+    private $response;
 
+    /**
+     * @param ResponseMessageType $response
+     */
+    public function setResponse(ResponseMessageType $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @return ResponseMessageType
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
 }
